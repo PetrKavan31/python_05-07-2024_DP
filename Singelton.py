41abbc3fcc335b862f331e366374fe4534feaea6
PetrKavan31 <petrkavan31@gmail.com>
2024-05-07 20:21:05 +0200

    json vs pickle

---
+++ Singelton.py
@@ -0,0 +1,18 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return  cls._instance
+
+    def __init__(self):
+        self.data = "Data of the Singleton instance"
+
+    def get_data(self):
+        return self.data
+
+
+singleton1 = Singleton()
+singleton2 = Singleton()
+
