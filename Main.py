d734b758893dc531d6259115b7b9a12fdf7f7008
PetrKavan31 <petrkavan31@gmail.com>
2024-05-07 19:01:59 +0200

    json vs pickle

---
+++ Main.py
@@ -0,0 +1,49 @@
+import pickle
+import json
+
+
+class Airplane:
+    def __init__(self, passengers, weight):
+        self.passengers = passengers
+        self.weight = weight
+
+    def __str__(self):
+        return f"{self.passengers}, {self.weight}"
+
+# file_namePKL = "airplane.pkl"
+# file_nameJSON = "airplane.json"
+
+    def pack_pickle(self):
+        return pickle.dumps(self)
+
+    @classmethod
+    def unpack_pickle(cls, pickle_data):
+        return pickle.loads(pickle_data)
+
+    def to_dict(self):
+        return {
+            "passengers": self.passengers,
+            "weight": self.weight,
+        }
+
+
+airplane1 = Airplane(150, 1500)
+
+
+def main():
+    # pickle file
+    pickle_data = airplane1.pack_pickle()
+    print(f"Saved data to pickle file: \n {pickle_data}")
+    from_pickle_airplane = Airplane.unpack_pickle(pickle_data)
+    print(f"Loaded data from pickle file: \n {from_pickle_airplane.__dict__}\n")
+
+    # json file
+    serialized = json.dumps(airplane1.to_dict())
+    print(f"Serialized object:\n\n{serialized}\n\n")
+
+    deserialized = json.loads(serialized)
+    print(f"Deserialized object:\n\n{deserialized}")
+
+
+if __name__ == "__main__":
+    main()
