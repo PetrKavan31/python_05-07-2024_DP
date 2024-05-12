e90b2329e3e6ac2359ab52175efe5f1b31438904
PetrKavan31 <petrkavan31@gmail.com>
2024-05-12 13:53:47 +0200

    all singleton

--- Singelton.py
+++ Singelton.py
@@ -13,6 +13,17 @@ class Singleton:
         return self.data
 
 
+# Testování Singleton vzoru
 singleton1 = Singleton()
 singleton2 = Singleton()
 
+print("Is singleton1 the same instance as singleton2?", singleton1 is singleton2)  # Výstup: True
+
+print("Data of singleton1:", singleton1.get_data())  # Výstup: Data of the Singleton instance
+print("Data of singleton2:", singleton2.get_data())  # Výstup: Data of the Singleton instance
+
+# Modifikace dat na jedné instanci
+singleton1.data = "Modified data"
+
+print("Data of singleton1:", singleton1.get_data())  # Výstup: Modified data
+print("Data of singleton2:", singleton2.get_data())  # Výstup: Modified data
